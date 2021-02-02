897611f31854e366b228d9d99b23b8ba622c7f01
Mario Finelli <mario@finel.li>
2021-02-01 21:08:45 -0500

    Start building main program loop

--- src/main.rs
+++ src/main.rs
@@ -8,164 +8,201 @@ use detect_desktop_environment::DesktopEnvironment;
 
 use tramonto::config::Config;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let config: Config;
     let configdir = dirs::config_dir();
 
     match configdir {
         Some(mut cd) => {
-            // println!("found {:?}", cd.into_os_string().into_string());
             cd.push("tramonto.yml");
-            // println!("{:?}", cd);
-            let config = Config::from_file(cd.to_str().unwrap());
-            println!("{:?}", config);
+            config = Config::from_file(cd.to_str().unwrap())?;
         },
         None => {
-            println!("no configdir, exit!");
+            return Err("unable to determine config directory")?;
+        }
+    };
+
+    {
+        let de = DesktopEnvironment::detect();
+        if de != DesktopEnvironment::Xfce {
+            return Err(format!("{:?} is not supported at this time", de))?;
         }
     }
-
-    std::process::exit(1);
-
-    let de: DesktopEnvironment = DesktopEnvironment::detect();
-
-    if de == DesktopEnvironment::Xfce {
-        println!("de is ok");
-    } else {
-        panic!("de is not supported!");
-    }
-
-    // let args: Vec<String> = env::args().collect();
-    //
-    let response = reqwest::get("https://ipinfo.io/json")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-
-    // println!("{:?}", response);
-    // println!("loc={}", response.get("loc").unwrap());
-    //
-    let loc: Vec<&str> = response.get("loc").unwrap().split(',').collect();
-    // println!("{:?}", loc);
-    //
-    if loc.len() != 2 {
-        panic!("didn't get two for lat/lng");
-    }
-
-    let url = format!(
-        "https://api.sunrise-sunset.org/json?lat={}&lng={}&formatted=0",
-        loc[0], loc[1]
-    );
-    // println!("{}", url);
-
-    let ss_response = reqwest::get(&url)
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-
-    // println!("{:?}", ss_response);
-    println!(
-        "sunup: {}, sundown: {}",
-        ss_response["results"]["sunrise"], ss_response["results"]["sunset"]
-    );
-    //
-
-    let sunup =
-        DateTime::parse_from_rfc3339(ss_response["results"]["sunrise"].as_str().unwrap()).unwrap();
-    let sundown =
-        DateTime::parse_from_rfc3339(ss_response["results"]["sunset"].as_str().unwrap()).unwrap();
-    println!("{:?}", sunup);
-    println!("{:?}", sundown);
-
-    let mut now: DateTime<Utc> = Utc::now();
-    println!("{:?}", now);
-
-    let nt = now.timestamp();
-    let upt = sunup.timestamp();
-    let downt = sundown.timestamp();
-    let lastchecked = now.day();
-
-    println!("now: {}, up: {}, down: {}", nt, upt, downt);
-
-    let tomorrowt = Utc.timestamp(nt + 86_400, 0);
-    // println!("{:?}", tomorrowt);
-    let tomorrow = Utc
-        .ymd(tomorrowt.year(), tomorrowt.month(), tomorrowt.day())
-        .and_hms(0, 0, 10);
-    println!("{:?}", tomorrow);
-
-    loop {
-        now = Utc::now();
-
-        if now.day() != lastchecked {
-            println!("it's a new day");
-        } else {
-            println!("it's the same day");
-        }
-
-        let timeofday =
-            tramonto::what_time_is_it(now, sunup.with_timezone(&Utc), sundown.with_timezone(&Utc));
-
-        match timeofday {
-            tramonto::TimeOfDay::PreDawn => {
-                println!("lib function predawn");
-                let output = Command::new("xfconf-query")
-                    .arg("-c")
-                    .arg("xsettings")
-                    .arg("-p")
-                    .arg("/Net/ThemeName")
-                    .arg("-s")
-                    .arg("Matcha-dark-azul")
-                    .output()
-                    .expect("failed to update theme");
-            }
-            tramonto::TimeOfDay::Daytime => {
-                println!("lib function daytime");
-
-                let output = Command::new("xfconf-query")
-                    .arg("-c")
-                    .arg("xsettings")
-                    .arg("-p")
-                    .arg("/Net/ThemeName")
-                    .arg("-s")
-                    .arg("Matcha-light-azul")
-                    .output()
-                    .expect("failed to update theme");
-            }
-            tramonto::TimeOfDay::PostDusk => {
-                println!("lib function postdusk");
-
-                let output = Command::new("xfconf-query")
-                    .arg("-c")
-                    .arg("xsettings")
-                    .arg("-p")
-                    .arg("/Net/ThemeName")
-                    .arg("-s")
-                    .arg("Matcha-dark-azul")
-                    .output()
-                    .expect("failed to update theme");
-            }
-        };
-
-        if upt > nt {
-            println!("it's before dawn");
-            println!("apply dark mode");
-            println!("sleep for: {}", upt - nt);
-        } else if upt <= nt || nt <= downt {
-            println!("it's between dawn and dusk");
-            println!("apply light mode");
-            println!("sleep for: {}", downt - nt);
-        } else {
-            println!("it's after dusk");
-            println!("apply dark mode");
-            println!("sleep for: {}", tomorrow.timestamp() - nt);
-        }
-
-        let one_minute = std::time::Duration::from_secs(60);
-        thread::sleep(one_minute);
-    }
-    // println!("{}", nt - upt);
-    // println!("{}", nt - downt);
 
     Ok(())
 }
+
+fn main() {
+    std::process::exit(match run() {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("error: {:?}", err);
+            1
+        }
+    });
+}
+
+//#[tokio::main]
+//async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//    let config: Config;
+//    let configdir = dirs::config_dir();
+
+//    match configdir {
+//        Some(mut cd) => {
+//            cd.push("tramonto.yml");
+//            config = Config::from_file(cd.to_str().unwrap()).unwrap_or_else(|err| {
+//                eprintln!("error: {}", err);
+//                process::exit(1);
+//            });
+//            println!("{:?}", config);
+//        },
+//        None => {
+//            eprintln!("error: unable to determine config directory!");
+//            process::exit(1);
+//        }
+//    }
+
+//    std::process::exit(1);
+
+//    let de: DesktopEnvironment = DesktopEnvironment::detect();
+
+//    if de == DesktopEnvironment::Xfce {
+//        println!("de is ok");
+//    } else {
+//        panic!("de is not supported!");
+//    }
+
+//    // let args: Vec<String> = env::args().collect();
+//    //
+//    let response = reqwest::get("https://ipinfo.io/json")
+//        .await?
+//        .json::<HashMap<String, String>>()
+//        .await?;
+
+//    // println!("{:?}", response);
+//    // println!("loc={}", response.get("loc").unwrap());
+//    //
+//    let loc: Vec<&str> = response.get("loc").unwrap().split(',').collect();
+//    // println!("{:?}", loc);
+//    //
+//    if loc.len() != 2 {
+//        panic!("didn't get two for lat/lng");
+//    }
+
+//    let url = format!(
+//        "https://api.sunrise-sunset.org/json?lat={}&lng={}&formatted=0",
+//        loc[0], loc[1]
+//    );
+//    // println!("{}", url);
+
+//    let ss_response = reqwest::get(&url)
+//        .await?
+//        .json::<serde_json::Value>()
+//        .await?;
+
+//    // println!("{:?}", ss_response);
+//    println!(
+//        "sunup: {}, sundown: {}",
+//        ss_response["results"]["sunrise"], ss_response["results"]["sunset"]
+//    );
+//    //
+
+//    let sunup =
+//        DateTime::parse_from_rfc3339(ss_response["results"]["sunrise"].as_str().unwrap()).unwrap();
+//    let sundown =
+//        DateTime::parse_from_rfc3339(ss_response["results"]["sunset"].as_str().unwrap()).unwrap();
+//    println!("{:?}", sunup);
+//    println!("{:?}", sundown);
+
+//    let mut now: DateTime<Utc> = Utc::now();
+//    println!("{:?}", now);
+
+//    let nt = now.timestamp();
+//    let upt = sunup.timestamp();
+//    let downt = sundown.timestamp();
+//    let lastchecked = now.day();
+
+//    println!("now: {}, up: {}, down: {}", nt, upt, downt);
+
+//    let tomorrowt = Utc.timestamp(nt + 86_400, 0);
+//    // println!("{:?}", tomorrowt);
+//    let tomorrow = Utc
+//        .ymd(tomorrowt.year(), tomorrowt.month(), tomorrowt.day())
+//        .and_hms(0, 0, 10);
+//    println!("{:?}", tomorrow);
+
+//    loop {
+//        now = Utc::now();
+
+//        if now.day() != lastchecked {
+//            println!("it's a new day");
+//        } else {
+//            println!("it's the same day");
+//        }
+
+//        let timeofday =
+//            tramonto::what_time_is_it(now, sunup.with_timezone(&Utc), sundown.with_timezone(&Utc));
+
+//        match timeofday {
+//            tramonto::TimeOfDay::PreDawn => {
+//                println!("lib function predawn");
+//                let output = Command::new("xfconf-query")
+//                    .arg("-c")
+//                    .arg("xsettings")
+//                    .arg("-p")
+//                    .arg("/Net/ThemeName")
+//                    .arg("-s")
+//                    .arg("Matcha-dark-azul")
+//                    .output()
+//                    .expect("failed to update theme");
+//            }
+//            tramonto::TimeOfDay::Daytime => {
+//                println!("lib function daytime");
+
+//                let output = Command::new("xfconf-query")
+//                    .arg("-c")
+//                    .arg("xsettings")
+//                    .arg("-p")
+//                    .arg("/Net/ThemeName")
+//                    .arg("-s")
+//                    .arg("Matcha-light-azul")
+//                    .output()
+//                    .expect("failed to update theme");
+//            }
+//            tramonto::TimeOfDay::PostDusk => {
+//                println!("lib function postdusk");
+
+//                let output = Command::new("xfconf-query")
+//                    .arg("-c")
+//                    .arg("xsettings")
+//                    .arg("-p")
+//                    .arg("/Net/ThemeName")
+//                    .arg("-s")
+//                    .arg("Matcha-dark-azul")
+//                    .output()
+//                    .expect("failed to update theme");
+//            }
+//        };
+
+//        if upt > nt {
+//            println!("it's before dawn");
+//            println!("apply dark mode");
+//            println!("sleep for: {}", upt - nt);
+//        } else if upt <= nt || nt <= downt {
+//            println!("it's between dawn and dusk");
+//            println!("apply light mode");
+//            println!("sleep for: {}", downt - nt);
+//        } else {
+//            println!("it's after dusk");
+//            println!("apply dark mode");
+//            println!("sleep for: {}", tomorrow.timestamp() - nt);
+//        }
+
+//        let one_minute = std::time::Duration::from_secs(60);
+//        thread::sleep(one_minute);
+//    }
+//    // println!("{}", nt - upt);
+//    // println!("{}", nt - downt);
+
+//    Ok(())
+//}
