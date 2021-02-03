379142632bfb7e0a14f620f4510bb380ac814f78
Mario Finelli <mario@finel.li>
2021-02-02 21:22:07 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 use chrono::prelude::*;
-// use std::collections::HashMap;
-// // use std::env;
-// use std::process::Command;
 use std::thread;
 
 use detect_desktop_environment::DesktopEnvironment;
@@ -49,7 +46,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
                 let loc = tramonto::ip::get_lat_lng().unwrap();
                 println!("{:?}", loc);
-                suninfo = tramonto::sun::SunInfo::from_api(loc.0, loc.1).unwrap();
+                suninfo = SunInfo::from_api(loc.0, loc.1).unwrap();
                 println!("{:?}", suninfo);
             }
 
@@ -60,20 +57,20 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
             ) {
                 tramonto::TimeOfDay::PreDawn => (
                     config.dark(),
-                    suninfo.sunup().timestamp() - Utc::now().timestamp(),
+                    suninfo.sunup().timestamp() - now.timestamp() + 30,
                 ),
                 tramonto::TimeOfDay::Daytime => (
                     config.light(),
-                    suninfo.sundown().timestamp() - Utc::now().timestamp(),
+                    suninfo.sundown().timestamp() - now.timestamp() + 30,
                 ),
                 tramonto::TimeOfDay::PostDusk => {
-                    let tomorrow = Utc.timestamp(Utc::now().timestamp() + 86_400, 0);
+                    let tomorrow = Utc.timestamp(now.timestamp() + 86_400, 0);
                     let timestamp = Utc
                         .ymd(tomorrow.year(), tomorrow.month(), tomorrow.day())
-                        .and_hms(0, 0, 10)
+                        .and_hms(0, 0, 0)
                         .timestamp();
 
-                    (config.dark(), timestamp - Utc::now().timestamp())
+                    (config.dark(), timestamp - now.timestamp() + 30)
                 }
             };
 
@@ -82,8 +79,6 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
                 _ => (),
             };
 
-            // println!("waiting: {}", whatdo.1);
-            // wait = std::time::Duration::from_secs(15);
             wait = std::time::Duration::from_secs(whatdo.1 as u64);
         }
 
@@ -102,99 +97,3 @@ fn main() {
         }
     });
 }
-
-//#[tokio::main]
-//async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-//    let mut now: DateTime<Utc> = Utc::now();
-//    println!("{:?}", now);
-
-//    let nt = now.timestamp();
-//    let upt = sunup.timestamp();
-//    let downt = sundown.timestamp();
-//    let lastchecked = now.day();
-
-//    println!("now: {}, up: {}, down: {}", nt, upt, downt);
-
-//    let tomorrowt = Utc.timestamp(nt + 86_400, 0);
-//    // println!("{:?}", tomorrowt);
-//    let tomorrow = Utc
-//        .ymd(tomorrowt.year(), tomorrowt.month(), tomorrowt.day())
-//        .and_hms(0, 0, 10);
-//    println!("{:?}", tomorrow);
-
-//    loop {
-//        now = Utc::now();
-
-//        if now.day() != lastchecked {
-//            println!("it's a new day");
-//        } else {
-//            println!("it's the same day");
-//        }
-
-//        let timeofday =
-//            tramonto::what_time_is_it(now, sunup.with_timezone(&Utc), sundown.with_timezone(&Utc));
-
-//        match timeofday {
-//            tramonto::TimeOfDay::PreDawn => {
-//                println!("lib function predawn");
-//                let output = Command::new("xfconf-query")
-//                    .arg("-c")
-//                    .arg("xsettings")
-//                    .arg("-p")
-//                    .arg("/Net/ThemeName")
-//                    .arg("-s")
-//                    .arg("Matcha-dark-azul")
-//                    .output()
-//                    .expect("failed to update theme");
-//            }
-//            tramonto::TimeOfDay::Daytime => {
-//                println!("lib function daytime");
-
-//                let output = Command::new("xfconf-query")
-//                    .arg("-c")
-//                    .arg("xsettings")
-//                    .arg("-p")
-//                    .arg("/Net/ThemeName")
-//                    .arg("-s")
-//                    .arg("Matcha-light-azul")
-//                    .output()
-//                    .expect("failed to update theme");
-//            }
-//            tramonto::TimeOfDay::PostDusk => {
-//                println!("lib function postdusk");
-
-//                let output = Command::new("xfconf-query")
-//                    .arg("-c")
-//                    .arg("xsettings")
-//                    .arg("-p")
-//                    .arg("/Net/ThemeName")
-//                    .arg("-s")
-//                    .arg("Matcha-dark-azul")
-//                    .output()
-//                    .expect("failed to update theme");
-//            }
-//        };
-
-//        if upt > nt {
-//            println!("it's before dawn");
-//            println!("apply dark mode");
-//            println!("sleep for: {}", upt - nt);
-//        } else if upt <= nt || nt <= downt {
-//            println!("it's between dawn and dusk");
-//            println!("apply light mode");
-//            println!("sleep for: {}", downt - nt);
-//        } else {
-//            println!("it's after dusk");
-//            println!("apply dark mode");
-//            println!("sleep for: {}", tomorrow.timestamp() - nt);
-//        }
-
-//        let one_minute = std::time::Duration::from_secs(60);
-//        thread::sleep(one_minute);
-//    }
-//    // println!("{}", nt - upt);
-//    // println!("{}", nt - downt);
-
-//    Ok(())
-//}
